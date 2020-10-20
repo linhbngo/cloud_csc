0905f409c3219c7fc527df0133ec28657a76fece
Linh B Ngo <lngo@wcupa.edu>
2020-10-20 13:43:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ set -x
 
 sudo apt-get update
 sudo sed -i 's/tcsh/bash/g' /etc/passwd
+
+# setup gdb-peda
+while IFS= read -r line; do
+  sudo su $line -c "cd ~; git clone https://github.com/longld/peda.git; echo 'source ~/peda/peda.py' > ~/.gdbinit"
+done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
